dfbb77bc15504cac78bbde76ed42e78b115e54ef
oodennis20 <oodennis20@gmail.com>
2019-11-17 01:42:32 +0300

    link data is diplaying

--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from app import app
-from .request import get_source,article_source, get_source
-
+from .request import get_source,article_source, get_category
+from flask import render_template,request,redirect,url_for
 #our views
 @app.route('/')
 def index():
@@ -21,17 +21,18 @@ def article(source_id):
     articles = article_source(id)
     return render_template('article.html',articles= articles,id=id)
 
-@app.route('/category/<cat_name>')
+@app.route('/categories/<cat_name>')
 def category(cat_name):
 
     '''
     View categories that return articles relevant to the category
     '''
     category = get_category(cat_name)
-    print (category)
+    # print (category)
     title = f'{cat_name}'
+    m=cat_name
     # source = get_categories()
-    return render_template('category.html',title = title,category=category)
+    return render_template('category.html',title = title,m=m, category=category)
 
 
 
